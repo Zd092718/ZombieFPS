57a3061a61de4bbe9778d8e8afd12adef87c6bcf
Zack Dowd <zdowd2796@gmail.com>
2023-03-21 16:00:34 -0600

    added camera look and adjusted mouse and gamepad sensitivity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,8 +11,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask groundLayerMask;
     private Vector2 moveComposite;
 
+    [Header("Look")]
+    [SerializeField] private Transform cameraContainer;
+    [SerializeField] private float minXLook;
+    [SerializeField] private float maxXLook;
+    [SerializeField] private bool invertCamera;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private float gamepadSensitivity;
+    private float lookSensitivity;
+    private float camCurXRot;
+    private Vector2 mouseDelta;
 
-    [SerializeField] private Vector2 lookComposite;
+
+    private PlayerInput playerInput;
     private Rigidbody rb;
     private CapsuleCollider capsule;
 
@@ -20,13 +31,22 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         capsule = GetComponent<CapsuleCollider>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
-
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     private void FixedUpdate()
     {
         Move();
+    }
+
+    private void LateUpdate()
+    {
+        CameraLook();
     }
 
     private void Move()
@@ -52,9 +72,49 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public bool IsGrounded()
+    {
+        RaycastHit hit;
+        if(Physics.SphereCast(transform.position, capsule.radius, Vector3.down, out hit,
+            (capsule.height / 2f) - capsule.radius + 0.1f))
+        {
+            return true;
+        }
+
+        return false;
+    }
+    private void CameraLook()
+    {
+        if (playerInput.currentControlScheme == "Gamepad")
+        {
+            lookSensitivity = gamepadSensitivity;
+        } else
+        {
+            lookSensitivity = mouseSensitivity;
+        }
+
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+
+        //Invert camera if option is selected
+        if (invertCamera)
+        {
+            cameraContainer.localEulerAngles = new Vector3(camCurXRot, 0, 0);
+
+        }
+        else
+        {
+            cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+        }
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+
+        Debug.Log(playerInput.currentControlScheme);
+    }
+
     public void OnLook(InputAction.CallbackContext context)
     {
-
+     
+        mouseDelta = context.ReadValue<Vector2>();
     }
 
     public void OnJump(InputAction.CallbackContext context)
@@ -70,15 +130,4 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public bool IsGrounded()
-    {
-        RaycastHit hit;
-        if(Physics.SphereCast(transform.position, capsule.radius, Vector3.down, out hit,
-            (capsule.height / 2f) - capsule.radius + 0.1f))
-        {
-            return true;
-        }
-
-        return false;
-    }
 }
