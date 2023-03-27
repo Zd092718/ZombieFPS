898894def8434d01b62324db738398583ddfbd8e
Zack Dowd <zdowd2796@gmail.com>
2023-03-27 15:57:01 -0600

    added ammo magazine mechanics and damage mechanics

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -6,18 +6,6 @@ public class SoundController : MonoBehaviour
 {
     public AudioSource shot;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Fire()
     {
         shot.Play();
